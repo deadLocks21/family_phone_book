f6356c1f8094f6204a56330d9eb733f85e61315d
Timothé HOFMANN <timothe@logomotion.fr>
2022-01-24 19:24:33 +0100

    Auth 2

--- templates/Pages/login.php
+++ templates/Pages/login.php
@@ -1,7 +1,7 @@
 <?= $this->Html->css(['login']) ?>
 
-<form action="/login" >
+<form action="/login" method="post">
     <label for="password">Mot de passe</label>
-    <input id="password" type="password">
+    <input id="password" type="password" name="password">
     <input type="submit" value="Se connecter">
 </form>

e9258874d49ceafcf807fce28b3df3ee55b3aec6
Timothé HOFMANN <timothe@logomotion.fr>
2022-01-25 20:09:51 +0100

    Display password

--- templates/Pages/login.php
+++ templates/Pages/login.php
@@ -2,6 +2,6 @@
 
 <form action="/family_phone_book/login" method="post">
     <label for="password">Mot de passe</label>
-    <input id="password" type="password" name="password">
+    <input id="password" type="text" name="password">
     <input type="submit" value="Se connecter">
 </form>

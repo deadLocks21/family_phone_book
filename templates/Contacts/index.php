7a4e9052810bea9c1446adb63246db2e286f06f7
Timothé HOFMANN <timothe@logomotion.fr>
2022-01-25 18:57:08 +0100

    Index view

--- templates/Contacts/index.php
+++ templates/Contacts/index.php
@@ -1,48 +1,20 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\Contact[]|\Cake\Collection\CollectionInterface $contacts
- */
-?>
+<?= $this->Html->css(['index']) ?>
 <div class="contacts index content">
-    <?= $this->Html->link(__('New Contact'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+    <?= $this->Html->link(__('Nouveau contact'), ['action' => 'add'], ['class' => 'button float-right']) ?>
     <h3><?= __('Contacts') ?></h3>
-    <div class="table-responsive">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('name') ?></th>
-                    <th><?= $this->Paginator->sort('mail') ?></th>
-                    <th><?= $this->Paginator->sort('address') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($contacts as $contact): ?>
-                <tr>
-                    <td><?= $this->Number->format($contact->id) ?></td>
-                    <td><?= h($contact->name) ?></td>
-                    <td><?= h($contact->mail) ?></td>
-                    <td><?= h($contact->address) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $contact->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $contact->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $contact->id], ['confirm' => __('Are you sure you want to delete # {0}?', $contact->id)]) ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-    </div>
+</div>
+<!-- <div style="background-color: white; z-index: 10;height: 7em; display:block;"></div> -->
+<div class="tiles-container">
+    <?php foreach ($contacts as $contact) : ?>
+        <div class="content margin-bottom w-90-p">
+            <h3><?= h($contact->name) ?></h3>
+            <h4><?= h($contact->mail) ?></h4>
+            <h4><?= str_replace("\n", "<br>", h($contact->address)) ?></h4>
+            <!-- <td class="actions">
+                <?= $this->Html->link(__('View'), ['action' => 'view', $contact->id]) ?>
+                <?= $this->Html->link(__('Edit'), ['action' => 'edit', $contact->id]) ?>
+                <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $contact->id], ['confirm' => __('Are you sure you want to delete # {0}?', $contact->id)]) ?>
+            </td> -->
+        </div>
+    <?php endforeach; ?>
 </div>
